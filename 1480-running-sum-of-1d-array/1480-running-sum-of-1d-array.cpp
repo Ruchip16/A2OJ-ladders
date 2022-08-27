f518ff080dd2f3bb9d345ebc7d00f6d157bc7678
Ruchi Pakhle <72685035+Ruchip16@users.noreply.github.com>
2022-08-27 11:40:54 +0530

    Time: 4 ms (62.65%), Space: 8.5 MB (75.23%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -5,7 +5,6 @@ public:
         int sum = 0;
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
-            // ans[i]=sum+nums[i];
             ans.push_back(sum);
             
         }
